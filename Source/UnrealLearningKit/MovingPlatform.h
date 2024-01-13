39c816295be33cab554616d84f90640783efdd39
c62gao <c62gao@uwaterloo.ca>
2024-01-12 23:00:55 -0500

    Set up C++

--- Source/UnrealLearningKit/MovingPlatform.h
+++ Source/UnrealLearningKit/MovingPlatform.h
@@ -10,8 +10,8 @@ UCLASS()
 class UNREALLEARNINGKIT_API AMovingPlatform : public AActor
 {
 	GENERATED_BODY()
-	
-public:	
+
+public:
 	// Sets default values for this actor's properties
 	AMovingPlatform();
 
@@ -19,8 +19,11 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
-public:	
+public:
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
-
+	UPROPERTY(EditAnywhere)
+	FVector MyVector = FVector(1, 2, 3);
+	UPROPERTY(EditAnywhere)
+	float Myx = 0;
 };
